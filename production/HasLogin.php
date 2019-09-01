3aa858ac8cfaa082b6b842f86e402310ce43920e
gileadn <49280308+gileadn@users.noreply.github.com>
2019-09-01 15:59:19 +0300

    new

---
+++ production/HasLogin.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>HASKALA</title>
+    
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="../vendors/animate.css/animate.min.css" rel="stylesheet">
+    
+    <link href="../vendors/fullcalendar/dist/fullcalendar.min.css" rel="stylesheet">
+    <link href="../vendors/fullcalendar/dist/fullcalendar.print.css" rel="stylesheet" media="print">
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
+</head>
+
+<body class="login" dir="rtl">
+<div>
+    <div class="login_wrapper">
+        <div class="animate form login_form">
+            <section class="login_content">
+                <form>
+                    <h1>
+                        <img src="images/logo1.png" height="80" width="80"/>
+                        <div>HASKALA</div>
+                    </h1>
+                    <div>
+                        <input type="text" name="id" class="form-control" placeholder="הכנס ת.ז" required/>
+                    </div>
+                    <div class="fc-button-group">
+                        <button type="button" class="fc-listMonth-button fc-button fc-state-default fc-corner-right" style="margin:-2px;width: 54px;height: 24px;">רגיל</button>
+                        <button type="button" class="fc-agendaWeek-button fc-button fc-state-default no-margin" style="margin:-2px;width: 54px;height: 24px;">קתה</button>
+                        <button type="button" class="fc-agendaDay-button fc-button fc-state-default no-margin" style="margin:-2px;width: 54px;height: 24px;">תורנויות</button>
+                        <button type="button" class="fc-month-button fc-button fc-state-default fc-corner-left fc-state-active no-margin" style="margin:-2px;width: 54px;height: 24px;">מעקבים</button>
+                    </div>
+                    <div>
+                        <a class="btn btn-default submit" href="index.html" style="margin: 15px 0px 0px 0px;">היכנס</a>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="separator">
+                        <br />
+                    </div>
+                </form>
+            </section>
+        </div>
+
+                        <div class="clearfix"></div>
+                        <br />
+                    </div>
+                </form>
+            </section>
+        </div>
+    </div>
+</div>
+<!-- jQuery -->
+<script src="../vendors/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="../vendors/fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="../vendors/nprogress/nprogress.js"></script>
+<!-- FullCalendar -->
+<script src="../vendors/moment/min/moment.min.js"></script>
+<script src="../vendors/fullcalendar/dist/fullcalendar.min.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="../build/js/custom.min.js"></script>
+</body>
+</html>
+
